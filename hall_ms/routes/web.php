e9ac088c7d2f38a6b12b4950cef453070c126dcd
Kcyberh <kcyberh30@gmail.com>
2024-08-24 16:13:18 +0100

    added a new route

--- hall_ms/routes/web.php
+++ hall_ms/routes/web.php
@@ -37,7 +37,7 @@ Route::middleware(['auth', 'verified'])->group(function (){
     Route::resource('booking', BookingController::class);
     Route::post('booking', [BookingController::class, 'search'])->name('booking.search');
     Route::get('/rooms/{hall}/{gender}', [RoomController::class, 'getRooms']);
-    
+    Route::get('/hall/{hall}/members', [HallController::class, 'members'])->name('hall.members');
 Route::get('/admin/register', [RegisteredUserController::class, 'showRegisterForm'])->name('admin.registerUser');
 Route::get('/admin/upload', [RegisteredUserController::class, 'showUploadform'])->name('admin.upload');
 Route::post('/import', [UserImportController::class, 'import'])->name('users.import');
@@ -59,6 +59,13 @@ Route::middleware(['auth','verified'])->group(function (){
    Route::post('resident', [ResidentController::class, 'search'])->name('resident.search');
    Route::post('resident/create', [ResidentController::class, 'store'])->name('resident.store');
    Route::delete('/residents/{resident}', [ResidentController::class, 'destroy'])->name('residents.destroy');
+});
+
+Route::middleware(['auth','verified'])->group(function(){
+    Route::get('callback',[PaymentController::class,'callback'])->name('callback');
+    Route::get('success',[PaymentController::class,'success'])->name('success');
+    Route::get('cancel',[PaymentController::class,'cancel'])->name('cancel');
+    Route::post('/save-order-and-pay', [PaymentController::class, 'initialize'])->name('payment.initialize');
 });
 
 //Route::get('/room', [RoomController::class, 'index'])->name('admin.index');
